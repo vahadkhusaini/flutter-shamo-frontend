fc2c931aa3f8de57eb55801086effd5a9e4e1a7f
Vahad Khusaini <vahadkhusaini@gmail.com>
2022-09-11 22:32:57 +0700

    learningpart wishlist page content

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/pages/widgets/wishlist_card.dart';
+import 'package:shamo/theme.dart';
 
 class WishlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Wishlist Page'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text('Favorite Shoes'),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget emptyWishList() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/image_wishlist.png',
+                width: 94,
+              ),
+              SizedBox(
+                height: 23,
+              ),
+              Text(
+                'You dont have a shoes?',
+                style:
+                    primaryTextStyle.copyWith(fontSize: 26, fontWeight: medium),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('data')
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+          child: Container(
+        color: backgroundColor1,
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          children: [
+            WishlistCard(),
+            WishlistCard(),
+            WishlistCard(),
+          ],
+        ),
+      ));
+    }
+
+    return Column(
+      children: [header(), content()],
     );
   }
 }
